5fe939dd1f6a02a573194d2971405bc1680bf082
ABKAM2023 <149762275+ABKAM2023@users.noreply.github.com>
2024-06-27 20:38:39 +0300

    1.1.0

---
+++ source/ConfigLoader.cs
@@ -0,0 +1,11 @@
+namespace LevelsRanksModuleNameReward
+{
+    public class NameRewardSettings
+    {
+        public string? Website { get; set; } = "example.com";
+        public double ExperienceMultiplier { get; set; } = 1.5;
+        public bool SendRoundStartMessages { get; set; } = true;
+    }
+
+}
+
